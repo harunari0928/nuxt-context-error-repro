d27d31396b55e8183535daf015fd453feca38e1e
harunari <ryo13579978@gmail.com>
2024-03-31 09:20:39 +0900

    Mock the import of symbol in the test setup

--- setup.ts
+++ setup.ts
@@ -3,3 +3,9 @@ import { expect } from "vitest";
 import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
+
+vi.mock("~/utils/symbol", () => {
+  return {
+    PROVIDES_SYMBOL: Symbol.for("my_provides"),
+  };
+});
